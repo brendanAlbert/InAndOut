1c47f239e1e19c7a823da0c816f67ae5319dc8e8
brendan <alber123@mail.chapman.edu>
2017-09-23 11:55:20 -0700

    Filled in activity_order.xml View.  Wired up OrderActivity.java Controller.

--- app/src/main/java/edu/orangecoastcollege/cs273/balbert/inandout/OrderActivity.java
+++ app/src/main/java/edu/orangecoastcollege/cs273/balbert/inandout/OrderActivity.java
@@ -1,13 +1,40 @@
 package edu.orangecoastcollege.cs273.balbert.inandout;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class OrderActivity extends AppCompatActivity {
+
+    EditText mDoubleDoubleEditText;
+    EditText mCheeseburgerEditText;
+    EditText mFrenchFriesEditText;
+    EditText mShakesEditText;
+    EditText mSmallDrinkEditText;
+    EditText mMediumDrinkEditText;
+    EditText mLargeDrinkEditText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
+
+        connectWidgets();
+    }
+
+    private void connectWidgets() {
+        mDoubleDoubleEditText = (EditText) findViewById(R.id.doubleDoubleEditText);
+        mCheeseburgerEditText = (EditText) findViewById(R.id.cheeseBurgerEditText);
+        mFrenchFriesEditText  = (EditText) findViewById(R.id.frenchFriesEditText);
+        mShakesEditText  = (EditText) findViewById(R.id.shakesEditText);
+        mSmallDrinkEditText  = (EditText) findViewById(R.id.smallDrinkEditText);
+        mMediumDrinkEditText  = (EditText) findViewById(R.id.mediumDrinkEditText);
+        mLargeDrinkEditText  = (EditText) findViewById(R.id.largeDrinkEditText);
+    }
+
+    private void placeOrder(View view) {
+
     }
 }
